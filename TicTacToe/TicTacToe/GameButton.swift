21c61425ac03bd7dcee21b9a584e925d9adc6bef
aglegaspi <lex83nyc@gmail.com>
2019-08-02 16:33:37 -0400

    removed everything affliated with GameButton...bye

--- TicTacToe/TicTacToe/GameButton.swift
+++ TicTacToe/TicTacToe/GameButton.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class GameButton: UIButton {
-  @IBInspectable var row: Int = 0
-  @IBInspectable var col: Int = 0
+    
+    @IBInspectable var row: Int = 0
+    @IBInspectable var col: Int = 0
+    
 }
