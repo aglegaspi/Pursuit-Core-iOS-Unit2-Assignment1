9db7088ac999c258fd2ca466a6fb0936f73c70ae
aglegaspi <lex83nyc@gmail.com>
2019-08-03 12:30:16 -0400

    starting over

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -10,11 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
-    @IBAction func buttonPressed(_ sender: UIButton) {
-        
-    }
-    
    
   override func viewDidLoad() {
     super.viewDidLoad()
