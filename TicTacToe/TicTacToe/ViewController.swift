e626f0c6d2f7ac47a666e329db4e1dbc445cac5b
aglegaspi <lex83nyc@gmail.com>
2019-08-04 10:16:16 -0400

    added all buttons and used switch case to update buttons

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -13,22 +13,45 @@ class ViewController: UIViewController {
     @IBOutlet weak var buttonOne: UIButton!
     @IBOutlet weak var buttonTwo: UIButton!
     @IBOutlet weak var buttonThree: UIButton!
+    @IBOutlet weak var buttonFour: UIButton!
+    @IBOutlet weak var buttonFive: UIButton!
+    @IBOutlet weak var buttonSix: UIButton!
+    @IBOutlet weak var buttonSeven: UIButton!
+    @IBOutlet weak var buttonEight: UIButton!
+    @IBOutlet weak var buttonNine: UIButton!
     
     @IBAction func buttonPressed(_ sender: UIButton) {
-        if sender.tag == 1 {
+        switch sender.tag {
+            
+        case 1:
             self.buttonOne.setTitle("X", for: .normal)
-        } else if sender.tag == 2 {
+        case 2:
             self.buttonTwo.setTitle("O", for: .normal)
-        } else if sender.tag == 3 {
+        case 3:
             self.buttonThree.setTitle("X", for: .normal)
+        case 4:
+            self.buttonFour.setTitle("O", for: .normal)
+        case 5:
+            self.buttonFive.setTitle("X", for: .normal)
+        case 6:
+            self.buttonSix.setTitle("O", for: .normal)
+        case 7:
+            self.buttonSeven.setTitle("X", for: .normal)
+        case 8:
+            self.buttonEight.setTitle("O", for: .normal)
+        case 9:
+            self.buttonNine.setTitle("X", for: .normal)
+        default:
+            self.buttonOne.setTitle("", for: .normal)
         }
+        
     }
-    
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view, typically from a nib.
+        }
+        
+        
 }
 
