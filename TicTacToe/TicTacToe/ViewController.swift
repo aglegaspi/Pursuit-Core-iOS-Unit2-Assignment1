9fed0c66a801669fc61c0c2c807f3205b7ee8bfe
aglegaspi <lex83nyc@gmail.com>
2019-08-04 10:07:04 -0400

    added functionality to update button three when pressed

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -12,12 +12,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var buttonOne: UIButton!
     @IBOutlet weak var buttonTwo: UIButton!
+    @IBOutlet weak var buttonThree: UIButton!
     
     @IBAction func buttonPressed(_ sender: UIButton) {
         if sender.tag == 1 {
             self.buttonOne.setTitle("X", for: .normal)
         } else if sender.tag == 2 {
             self.buttonTwo.setTitle("O", for: .normal)
+        } else if sender.tag == 3 {
+            self.buttonThree.setTitle("X", for: .normal)
         }
     }
     
